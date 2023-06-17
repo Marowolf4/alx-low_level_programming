742c0a9752dd469cc75ba79b1b563f3b5ee3822a
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-17 12:31:41 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,21 +2,30 @@
 #include <time.h>
 #include <stdio.h>
 
-
+/**
+ * main - it do da thingy
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char m = 'a';
+
 	int i = 0;
-	while (i < 26) {
+
+	while (i < 26)
+	{
 		putchar(m + i);
 		i++;
 	}
 	i = 0;
-	while (i < 26) {
+	while (i < 26)
+	{
 		char m = 'A';
+		
 		putchar(m + i);
-		if (i == 25) putchar('\n');
+		if (i == 25)
+			putchar('\n');
 		i++;
 	}
-	return(0);
+	return (0);
 }
