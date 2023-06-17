6a076da8a007f762d68742349c5711f921e72053
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-17 12:18:04 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*main is a function */
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 { /*main is a function */
 	int n;
