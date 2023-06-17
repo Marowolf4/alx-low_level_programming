b92242b4648a4a6fafb89b96b734764eb2ded222
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-17 12:53:47 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,15 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 
-
+/**
+ * main - it do da thingy
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char m = 'a';
+
 	int i = 25;
-	while (i >= 0) {
+
+	while (i >= 0)
+	{
 		putchar(m + i);
 		i--;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
