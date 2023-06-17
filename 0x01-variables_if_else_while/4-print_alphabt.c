29009e95a0513fc37403d402396cffc10291267f
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-17 12:48:36 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,17 +2,25 @@
 #include <time.h>
 #include <stdio.h>
 
-
+/**
+ * main - it do da thingy
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char m = 'a';
+
 	int i = 0;
-	while (i < 26) {
-		if (i != 16 && i != 4) {
+
+	while (i < 26)
+	{
+		if (i != 16 && i != 4)
+		{
 			putchar(m + i);
-			if (i == 25) putchar('\n');
+			if (i == 25)
+				putchar('\n');
 		}
 		i++;
 	}
-	return(0);
+	return (0);
 }
