d4ac8b2958543906d3c897f4d7865aa804158cab
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-17 12:54:27 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,19 +2,25 @@
 #include <time.h>
 #include <stdio.h>
 
-
+/**
+ * main - it do da thingy
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i = 48;
-	while (i < 58) {
+
+	while (i < 58)
+	{
 		putchar(i);
 		i++;
 	}
 	i = 97;
-	while (i < 103) {
+	while (i < 103)
+	{
 		putchar(i);
 		i++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
