225335414f799af6d95568ba6b3db4b291d03afd
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-17 03:02:02 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+	int i = 48;
+	while (i < 58) {
+		putchar(i);
+		i++;
+	}
+	i = 97;
+	while (i < 103) {
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+}
