7d9289f8fbb02f3a7db8f40b27a476b93866fbaa
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-17 12:20:34 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-
+/**
+ * main - it do da thingy
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -19,5 +22,5 @@ int main(void)
 		else if (m == 0) printf("Last digit of %d is %d and is 0\n", n, m);
 		else if (m < 6) printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
-	return(0);
+	return (0);
 }
