a2f5d5b3377047fa47096428f679b51ebb323b63
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-19 02:50:07 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - it do da thingy
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c = 48;
+	printf("%c", c);
+
+	int i = 48;
+	int j = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58) 
+		{
+			if (i != j) {
+				if (i * 2 + j < j * 2 + i) {
+					if (i != 0 && j != 0) {
+						putchar(',');
+						putchar(' ');
+					}
+					putchar(i);
+					putchar(j);
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
