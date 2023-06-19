45aae516f2ee69fe5d2371620b121855b2a1a417
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-19 03:06:39 +0300

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - it do da thingy
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int k = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58) 
+		{
+			k = 48;
+			while (k < 58)
+			{
+				if (i != j && i != k && j != k)
+				{
+					if (i * 3 + j * 2 + k < k * 3 + j * 2 + i && i * 3 + j * 2 + k < j * 3 + k * 2 + i && i * 3 + j * 2 + k < j * 3 + i * 2 + k && i * 3 + j * 2 + k < i * 3 + k * 2 + j)
+					{
+						if (!(i == 48 && j == 49 && k == 50))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+						putchar(i);
+						putchar(j);
+						putchar(k);
+					}
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
