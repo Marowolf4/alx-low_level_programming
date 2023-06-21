4423edafc9b9ac48f4c6e86a33d7e49a7b3cbe75
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-21 05:44:37 +0300

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+* main - it do da thingy
+* Return: Always 0 (Success)
+*/
+
+int main()
+{
+	  printf("_putchar\n");
+    return 0;
+}
+
