24878be297f1b0e83812c0cfe591e9f00e961ec2
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 02:03:39 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,55 +8,55 @@
 
 void jack_bauer(void)
 {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int l = 0;
+int i = 0;
+int j = 0;
+int k = 0;
+int l = 0;
 
-    while (i < 2)
-    {
-        j = 0;
-        while (j < 10)
-        {
-            k = 0;
-            while (k < 6)
-            {
-                l = 0;
-                while (l < 10)
-                {
-                    _putchar(i + '0');
-                    _putchar(j + '0');
-                    _putchar(':');
-                    _putchar(k + '0');
-                    _putchar(l + '0');
-                    _putchar('\n');
-                    l++;
-                }
-                k++;
-            }
-            j++;
-        }
-        i++;
-    }
-    j = 0;
-    while (j < 4)
-        {
-        k = 0;
-        while (k < 6)
-        {
-            l = 0;
-            while (l < 10)
-            {
-                _putchar(i + '0');
-                _putchar(j + '0');
-                _putchar(':');
-                _putchar(k + '0');
-                _putchar(l + '0');
-                _putchar('\n');
-                l++;
-            }
-            k++;
-        }
-        j++;
-    }
+while (i < 2)
+{
+j = 0;
+while (j < 10)
+{
+k = 0;
+while (k < 6)
+{
+l = 0;
+while (l < 10)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
+l++;
+}
+k++;
+}
+j++;
+}
+i++;
+}
+j = 0;
+while (j < 4)
+{
+k = 0;
+while (k < 6)
+{
+l = 0;
+while (l < 10)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
+l++;
+}
+k++;
+}
+j++;
+}
 }
