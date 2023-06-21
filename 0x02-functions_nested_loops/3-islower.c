04056af94e404a6feea576cf77022fa5bcce4d43
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 00:53:46 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+* _islower - does my stuff
+* Description: Does my stuff explained.
+* Return: always 0 always void
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
