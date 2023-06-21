49135557a2a8b9ead105a3f7109d7b3ebc52b14c
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-21 07:53:10 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int a = 97;
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+}
