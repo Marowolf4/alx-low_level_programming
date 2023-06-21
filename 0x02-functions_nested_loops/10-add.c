a34bb87bbc4955fbaca51332d334855311c8113e
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 02:33:43 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - does my stuff
+* @x : your mom
+* @y : your dad
+* Description: Does my stuff explained.
+* Return: always 0 always void
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
