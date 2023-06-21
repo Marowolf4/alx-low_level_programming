6d5495f8df9d75e9e49651873cc3be69d2a5c4ce
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 01:01:36 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _isalpha - does my stuff
+* @c: its mine damnit
+* Description: Does my stuff explained.
+* Return: always 0 always void
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c < 123)
+	{
+		return (1);
+	}
+	else if (c >=65 && c <91)
+	{
+		return (1);
+	} 
+	else
+	{
+		return (0);
+	}
+}
