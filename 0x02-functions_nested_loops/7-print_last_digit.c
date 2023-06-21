49af73e42f9a33a8b78d15b6535bd71c608d2ab4
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 01:31:36 +0300

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - does my stuff
+* @n: its mine damnit
+* Description: Does my stuff explained.
+* Return: always 0 always void
+*/
+int print_last_digit(int n)
+{
+	int f = n % 10;
+
+	_putchar(f +'0');
+	return(f);
+}
