ce8f3c63a56347d2163720b5f77935e812e8b8ac
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 00:42:16 +0300

    Update _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+ /**
+* print_last_digit - does my stuff
+* @n: its mine damnit
+* Description: Does my stuff explained.
+* Return: always 0 always void
+*/
 int _putchar(char n)
 {
 	putchar(n);
