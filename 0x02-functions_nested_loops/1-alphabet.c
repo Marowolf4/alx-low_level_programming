8cfb7767c231002fab1a926af85f7acac794f0f0
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 00:44:12 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 
 /**
 * print_alphabet - does my stuff
-* @n: its mine damnit
 * Description: Does my stuff explained.
 * Return: always 0 always void
 */
 void print_alphabet(void)
 {
 	int a = 97;
+
 	while (a < 123)
 	{
 		putchar(a);
