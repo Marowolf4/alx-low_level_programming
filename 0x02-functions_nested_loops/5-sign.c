40095d8372c7584608b5fbd45ea6bf3775649d78
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 01:13:10 +0300

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - does my stuff
+* @n: its mine damnit
+* Description: Does my stuff explained.
+* Return: always 0 always void
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+}
