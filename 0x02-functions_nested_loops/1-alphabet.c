2c85d0aed5a23bffda9aafaa52d79716fd5ee3e0
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-21 07:43:42 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	int a = 97;
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
 }
