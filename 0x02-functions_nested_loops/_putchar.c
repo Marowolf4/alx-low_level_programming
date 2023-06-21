f24985203600ca260cfcae6a16d58b9543343c0b
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 00:43:07 +0300

    Update _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
  /**
-* print_last_digit - does my stuff
+* _putchar - does my stuff
 * @n: its mine damnit
 * Description: Does my stuff explained.
 * Return: always 0 always void
