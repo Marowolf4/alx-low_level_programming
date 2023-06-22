cc23ecf33e79c0d494c094ca3cadf81addb128c7
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-22 08:49:34 +0300

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - does my stuff
+* Description: Does my stuff explained.
+* Return: always 0 always void
+*/
+void main(void)
+{
+	int i = 0;
+	int j = 3;
+	int k = 2;
+	int l = 1;
+
+	printf("0, 1, 2");
+	while (i < 97)
+	{
+		j = abs(l + k);
+		printf(", %d", abs(j));
+		l = abs(k);
+		k = abs(j);
+		k = abs(j);
+		i++;
+	}
+	putchar('\n');
+}
