517e25e5510598332bf7e9fe829dbc74b4a3d749
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-24 05:41:28 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - check the code.
+ * @n: kys
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
