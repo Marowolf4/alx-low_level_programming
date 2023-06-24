9deb16ffc9e9f4bc459f9cef17cb56ea9c205f55
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-24 05:26:29 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code.
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
