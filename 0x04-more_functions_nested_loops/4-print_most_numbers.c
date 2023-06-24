346c1d70b3bbbe2acc1f8e3529e83235a03a1e38
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-24 05:31:53 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include main.h
+
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+void print_most_numbers(void);
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+			putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+}
