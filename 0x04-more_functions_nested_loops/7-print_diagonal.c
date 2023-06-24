4524da9090ea66b5fa3d0c81fd8d2822074f79fa
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-24 06:03:51 +0300

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code.
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < n)
+	{
+		j = 0;
+		while (j < i)
+		{
+			putchar(' ');
+			j++;
+		}
+		putchar('\\');
+		putchar('\n');
+		i++;
+	}
+	putchar('\n');
+	putchar(' ');
+}
