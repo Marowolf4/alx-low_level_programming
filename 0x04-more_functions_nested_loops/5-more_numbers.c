90264096468eb9dda01a7bd1a7ace100c962ace8
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-24 05:37:46 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code.
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			printf("%d", j);
+			j++;
+	
+		}
+		putchar('\n');
+		i++;
+	}
+}
