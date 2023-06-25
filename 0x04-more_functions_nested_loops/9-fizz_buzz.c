484b7ef3879e4b78b8029a9406a385002f02f619
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-25 03:11:37 +0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main()
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		else
+			printf("%d", i);
+		putchar(' ');
+		i++;
+	}
+}
