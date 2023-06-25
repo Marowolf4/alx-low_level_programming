55756bc8cac885e333d39aefc35b3296c145abdf
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-25 04:31:39 +0300

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+ * main - check the code
+ * Return: Always 0.
+ */
+int main()
+{
+	long long int i = 2;
+	long long int n = 612852475143;
+
+	while (n / 2 > i)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+		}
+		else
+			i++;
+	}
+	printf("%d\n", n);
+  return (0);
+}
