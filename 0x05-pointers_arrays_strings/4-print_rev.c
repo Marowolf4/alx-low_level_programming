5dc6787ef0bd3e1d9ff5e367c97f3cd4ec32b7c4
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-27 06:00:13 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: kys
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	long unsigned int j = strlen(str);
+
+	while (j > 0)
+	{
+		putchar(i[j - 1]);
+		j--;
+	}
+	putchar('\n');
+}
