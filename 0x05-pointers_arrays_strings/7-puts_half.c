f2232d26a0becf611f0f416c4806d073c101f735
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-28 05:13:32 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str: kys
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	j = i;
+	i = i/2;
+	while (i <= j)
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
