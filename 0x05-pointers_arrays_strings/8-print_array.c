bcc66b873e670e985db773528386b3698cb3707d
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-28 05:31:27 +0300

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - check the code for
+ * @a: kys
+ * @n: kys
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i = 1;
+
+	printf("%d", a[0]);
+	while (i < n)
+	{
+		printf(", %d", a[i]);
+		i++;
+	}
+	putchar('\n');
+}
