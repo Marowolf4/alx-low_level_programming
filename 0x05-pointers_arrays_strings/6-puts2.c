3b8749ac557de5acd0a76d51c1a3cd3e59243f42
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-28 05:04:50 +0300

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ * @str: kys
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+		i++;
+	}
+}
