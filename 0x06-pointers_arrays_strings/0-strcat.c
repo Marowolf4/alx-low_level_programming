001226f45ff882cf3b897cd6b75bb08276c5e2f1
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-30 05:38:28 +0300

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - check the code
+ * @dest: kys
+ * @src: kys
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return dest;
+}
