c5f70c7d5e982d4ef933aee0bd0867156365cfc0
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-06-30 05:44:15 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
-	return dest;
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
 }
