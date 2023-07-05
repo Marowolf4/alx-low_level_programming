bfb75e1e24023aafe1df457a7ddb9c70d358c256
Marowolf4 <133545422+Marowolf4@users.noreply.github.com>
2023-07-05 03:43:57 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,6 +7,12 @@ include "main.h"
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	memset(s, b ,size);
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
 	return (s);
 }
